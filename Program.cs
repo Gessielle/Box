7783313b1ee6beae0c5c03fd6dc76155de6bbcb7
Gessielle <gessielle-barossi@hotmail.com>
2020-09-13 21:00:28 -0300

    Caixa com nome

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Box_com_Nome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(" +-----------+");
+            Console.WriteLine(" | Gessielle | ");
+            Console.WriteLine(" +-----------+ ");
+        }
+    }
+}
